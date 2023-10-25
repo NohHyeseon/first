644c55edd0c9d4a027bb7f7726c59292be513963
DoheumPark <pirbak@hanmail.net>
2023-10-25 17:04:26 +0900

    23/10/25 7교시

---
+++ src/main/java/com/green/day2/ch2/extra/StringEx.java
@@ -0,0 +1,53 @@
+package com.green.day2.ch2.extra;
+
+public class StringEx {
+    public static void main(String[] args) {
+        String name = "Ja" + "va";
+        System.out.println(name);
+
+        String title1 = "12" + 10 + 9;
+        //String title1 = "12" + "10" + 9;
+        //String title1 = "1210" + 9;
+        //String title1 = "1210" + "9";
+        //String title1 = "12109";
+        System.out.println(title1); //12109
+
+        String title2 = "12" + (10 + 9);
+        //String title2 = "12" + 19;
+        //String title2 = "12" + "19";
+        //String title2 = "1219";
+        System.out.println(title2); //1219
+
+
+
+
+
+
+        String str1 = new String("10");
+        String str2 = new String("Hello");
+        System.out.println("str1 == str2 : " + (str1 == str2));
+        System.out.println("str1.equals(str2) : " + str1.equals(str2));
+
+        Numbox b1 = new Numbox(12);
+        Numbox b2 = new Numbox(12);
+        //1.
+        int num1 = b1.getNum();
+        int num2 = b2.getNum();
+        System.out.println(num1 == num2);
+        System.out.println("b1 == b2 : " + (b1 == b2));
+        System.out.println("b1.equals(b2) : " + b1.equals(b2)); //true
+        System.out.println("b1.equals(str1) : " + b1.equals(str1)); //false
+        System.out.println("b1.equals(null) : " + b1.equals(null)); //false
+    }
+}
+class Numbox extends Object { //기본생성자 없음
+    private int num;
+    public Numbox(int num) { this.num = num; }
+    public int getNum() { return num; }
+    @Override
+    public boolean equals(Object obj) {
+        if(!(obj instanceof Numbox)) { return false; }
+        Numbox nb = (Numbox)obj;
+        return num == nb.num;
+    }
+}
---
+++ src/main/java/com/green/day3/ch4/extra/FlowEx1Result.java
@@ -0,0 +1,35 @@
+package com.green.day3.ch4.extra;
+
+import java.util.Scanner;
+
+public class FlowEx1Result {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("숫자를 하나 입력하세요 >> ");
+        String input = scan.nextLine();
+        try {
+            int intInput = Integer.parseInt(input);
+            System.out.printf("%d는(은) %s수입니다.\n"
+                    , intInput, intInput % 2 == 0 ? "짝" : "홀");
+        } catch (Exception e) {
+            System.out.println("잘못된 입력입니다.");
+        }
+    }
+}
+
+class FlowEx1Result2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("숫자를 하나 입력하세요 >> ");
+        String input = scan.nextLine();
+        int intVal = 0;
+        try {
+            intVal = Integer.parseInt(input);
+        } catch (Exception e) {
+            System.out.println("잘못된 입력입니다.");
+            return;
+        }
+        System.out.printf("%d는(은) %s수입니다.\n"
+                , intVal, intVal % 2 == 0 ? "짝" : "홀");
+    }
+}
