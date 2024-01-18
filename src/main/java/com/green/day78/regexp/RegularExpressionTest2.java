0f7816d6ca917e9bb8978a3f41c0ab683a705b7e
sunxno <sunxno@naver.com>
2024-01-18 12:45:35 +0900

    1818

--- src/main/java/com/green/day78/regexp/RegularExpressionTest2.java
+++ src/main/java/com/green/day78/regexp/RegularExpressionTest2.java
@@ -7,10 +7,13 @@ public class RegularExpressionTest2 {
         String str1 = "adfAdfdf";
         String str2 = "adfAdfdf OO";
         String str3 = "ABCD12";
+        String str4 = "ABCD12";
         String regexp = "^[a-z|A-Z]*$";
 
         System.out.printf("%s > %b\n", str1, Pattern.matches(regexp, str1));
         System.out.printf("%s > %b\n", str2, Pattern.matches(regexp, str2));
         System.out.printf("%s > %b\n", str3, Pattern.matches(regexp, str3));
+        System.out.printf("%s > %b\n", str4, Pattern.matches(regexp, str3));
+
     }
 }
